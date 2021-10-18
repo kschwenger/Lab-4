a7a001d4be494f31dada5924984474a194a1a75e
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-10-18 01:52:27 +0000

    updatign

--- Lab_4_cgi.py
+++ Lab_4_cgi.py
@@ -3,10 +3,6 @@ import cgi    # for radio
 print("Content-type: text/html\n\n")
 data = cgi.FieldStorage()
 print("selection = " + data.getvalue("option"))
-
-# for slider
-print("Content-type: text/html\n\n")
-data = cgi.FieldStorage()
 print("LEDslider = " + data.getvalue('slider') + '<br>')
 
 # sending data with jsom 
