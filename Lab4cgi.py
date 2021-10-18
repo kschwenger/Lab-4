d294505645b67d3bdd4ec8def9aca7845c491a4f
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-10-18 23:44:32 +0000

    trying stuff

--- Lab4cgi.py
+++ Lab4cgi.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python37all
-import cgi    
-print("Content-type: text/html\n\n")
+import cgi
 data = cgi.FieldStorage()
-print("selection = " + data.getvalue("option") + '<br>')
-print("LEDslider = " + data.getvalue('slider') + '<br>')
+selectedLED = data.getvalue('option')
+LEDvalue = data.getvalue('slider')
 
-"""
-# sending data with jsom 
-import json
-selectedLED = data.getvalue('selection')
-LEDvalue = data.getvalue('LEDslider')
-data = {"selection":selectedLED, "LEDslider":LEDvalue}
-with open('Lab4pwm.txt', 'w') as f:
-json.dump(data,f)
-"""
+print('Content-type: text/html\n\n')
+print('<html>')
+
+print('<form action="/cgi-bin/Lab4cgi.py" method="POST">')
+print('<input type="radio" name="option" value="LED 1"> LED 1 <br>')
+print('<input type="radio" name="option" value="LED 2"> LED 2 <br>')
+print('<input type="radio" name="option" value="LED 3"> LED 3 <br>')
+
+print('<input type="range" name="slider" min ="0" max="100" value ="LEDvalue"/><br>')
+
+print("selection = " + selectedLED + '<br>')
+print("LEDslider = " + LEDvalue + '<br>')
