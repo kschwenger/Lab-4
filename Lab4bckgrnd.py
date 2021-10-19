e5b948e3232789cd266f1d36b98c78a5ae41bf7e
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-10-19 00:26:58 +0000

    editas

--- Lab4bckgrnd.py
+++ Lab4bckgrnd.py
@@ -2,16 +2,19 @@ import json
 import RPi.GPIO as GPIO
 import time
 
+# establish leds
 led1 = 13
 led2 = 26
 led3 = 20
 
+# gpio set up
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(led1, GPIO.OUT)
 GPIO.setup(led2, GPIO.OUT)
 GPIO.setup(led3, GPIO.OUT)
 
-pwm1 = gpio.PWM(led1, 100) # create PWM objects @ 100 Hz for leds
+# create PWM objects @ 100 Hz for leds
+pwm1 = gpio.PWM(led1, 100)
 pwm2 = gpio.PWM(led2, 100)
 pwm3 = gpio.PWM(led3, 100)
 
