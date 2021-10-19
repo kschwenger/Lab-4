5e1e7d7d650a5c4074ed2aa3abd2106b3544ff12
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-10-19 00:02:41 +0000

    exception

--- Lab4bckgrnd.py
+++ Lab4bckgrnd.py
@@ -1,6 +1,11 @@
-# reading data with json
 import json
-with open('Lab4pwm.txt', 'r') as f:
-  data = json.load(f)
-print("selected LED = " + str(data['selection']))
-print("LED value = " + str(data['slider']))
+try:
+  while True:
+    with open('Lab4pwm.txt', 'r') as f:
+      data = json.load(f)
+      print("selected LED = " + str(data['selection']))
+      print("LED value = " + str(data['slider']))
+except KeyboardInterrupt: # if user hits ctrl-C
+  print('\nExiting')
+except Exception as e: # catch all other errors
+  print('\ne')
