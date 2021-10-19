05561af66547c12c1fbaf6319cf6132eecbf0682
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-10-19 01:17:16 +0000

    edits

--- Lab4bckgrnd.py
+++ Lab4bckgrnd.py
@@ -9,14 +9,15 @@ led3 = 20
 
 # gpio set up
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False) # ignore warnings due to multiple scripts at same time
 GPIO.setup(led1, GPIO.OUT)
 GPIO.setup(led2, GPIO.OUT)
 GPIO.setup(led3, GPIO.OUT)
 
 # create PWM objects @ 100 Hz for leds
-pwm1 = gpio.PWM(led1, 100)
-pwm2 = gpio.PWM(led2, 100)
-pwm3 = gpio.PWM(led3, 100)
+pwm1 = GPIO.PWM(led1, 100)
+pwm2 = GPIO.PWM(led2, 100)
+pwm3 = GPIO.PWM(led3, 100)
 
 try:
   while True:
--- Lab4cgi.py
+++ Lab4cgi.py
@@ -16,15 +16,15 @@ with open('Lab4pwm.txt', 'w') as f:
 print('Content-type: text/html\n\n')
 print('<html>')
 
+print("selection = " + selectedLED + '<br>')
+print("LEDslider = " + LEDvalue + '<br>')
+
 print('<form action="/cgi-bin/Lab4cgi.py" method="POST">')
 print('<input type="radio" name="option" value="LED 1"> LED 1 <br>')
 print('<input type="radio" name="option" value="LED 2"> LED 2 <br>')
 print('<input type="radio" name="option" value="LED 3"> LED 3 <br>')
 
 print('<input type="range" name="slider" min ="0" max="100" value ="LEDvalue"/><br>')
-
-print("selection = " + selectedLED + '<br>')
-print("LEDslider = " + LEDvalue + '<br>')
 
 print('<input type="submit" value="Submit">')
 print('</form>')
